27557c045995ef1c01d2a3bfd40fcfa4fdfea9b5
e-k-agentur <stefan.eideloth@gmail.com>
2023-01-20 15:06:10 +0100

    header & footer progress

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,27 +8,21 @@
 </script>
 
 <header
-  class="absolute top-0 z-20 w-full bg-zinc-800 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-800"
+  class="absolute top-0 z-20 w-full bg-zinc-100 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-100"
 >
   <nav
     class="container mx-auto py-4 px-6 md:flex md:h-20 md:items-center md:justify-between md:p-2"
   >
     <div class="flex items-center justify-between">
-      <a href="#/">
+      <a href="/#">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 200 40"
+          viewBox="0 0 60 40"
           class="h-10 fill-current"
         >
           <path
             d="M0 0h24.971v8.644H0V0Zm0 31.388v8.611h24.971v-8.611H0Zm10.872-7.02V15.71h17.747L44.318 0h12.091L31.986 24.368H10.872Zm26.274 4.878 6.166-6.12L60.22 39.999H47.944L37.146 29.246Z"
           />
-          <text
-            font-family="Poppins"
-            font-weight="bold"
-            transform="matrix(.08008 0 0 .08 57.895 23.883)"
-            font-size="200">SEK-Consulting</text
-          >
         </svg>
       </a>
       <button class="flex md:hidden" on:click={() => (showMenu = !showMenu)}>
@@ -41,7 +35,6 @@
     </div>
 
     <ul
-      id="nav-links"
       class="mt-8 flex-col items-center space-y-4 text-lg md:mt-0 md:flex md:flex-row md:space-y-0 md:space-x-10 
       {showMenu ? 'flex' : 'hidden'}"
     >
